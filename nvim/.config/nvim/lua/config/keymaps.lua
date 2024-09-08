cb46ff07b540e8f4de04e4fbe48c261a5be4cd02
Alex Beauchemin <alexbeauchemin01@gmail.com>
2024-09-08 17:21:46 -0400

    Add ctrl+backspace and alt+backspace behavior to delete word under cursor

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -14,3 +14,9 @@ end, { silent = true })
 -- Map M-l to M-CR for insert and normal mode
 -- to accept supermaven suggestion using alt+l in addition to alt+enter
 vim.keymap.set({ "i", "n" }, "<M-l>", "<M-CR>", { remap = true })
+
+-- Replicate the behavior of the ctrl+backspace/alt+backspace key in other softwares (delete word under the cursor)
+vim.keymap.set("n", "<C-BS>", "daw", { remap = true, silent = true })
+vim.keymap.set("i", "<C-BS>", "<esc>caw", { remap = true, silent = true })
+vim.keymap.set("n", "<M-BS>", "daw", { remap = true, silent = true })
+vim.keymap.set("i", "<M-BS>", "<esc>caw", { remap = true, silent = true })
