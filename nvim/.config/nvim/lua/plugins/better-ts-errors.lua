5634fd27d3b37129e645b34b38e0ed0f2ff5d201
Alex Beauchemin <alex.beauchemin@planned.com>
2024-09-10 22:52:50 -0400

    Add better-ts-errors to nvim and update dependencies

---
+++ nvim/.config/nvim/lua/plugins/better-ts-errors.lua
@@ -0,0 +1,10 @@
+return {
+  "OlegGulevskyy/better-ts-errors.nvim",
+  dependencies = { "MunifTanjim/nui.nvim" },
+  config = {
+    keymaps = {
+      toggle = "gk", -- default '<leader>dd'
+      go_to_definition = "<leader>dx", -- default '<leader>dx'
+    },
+  },
+}
