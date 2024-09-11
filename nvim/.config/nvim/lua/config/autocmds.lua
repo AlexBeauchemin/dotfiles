314c284a83fa9330224ab9cf06b85a294aeb9e49
Alex Beauchemin <alex.beauchemin@planned.com>
2024-09-10 22:53:08 -0400

    Attach twoslash-queries to vtsls

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,10 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- Attach twoslash-queries to vtsls
+require("lspconfig")["vtsls"].setup({
+  on_attach = function(client, bufnr)
+    require("twoslash-queries").attach(client, bufnr)
+  end,
+})
