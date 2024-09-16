78ea307c8f53ee018c26e7c33fca9c2c204e8c7f
Alex Beauchemin <alex.beauchemin@planned.com>
2024-09-16 14:33:30 -0400

    Fix better-ts-errors config, fix package-info not auto loading, update packages

--- nvim/.config/nvim/lua/plugins/package-info.lua
+++ nvim/.config/nvim/lua/plugins/package-info.lua
@@ -3,8 +3,13 @@ return {
   dependencies = {
     "MunifTanjim/nui.nvim",
   },
+  -- Only load when editing json files
+  ft = "json",
   config = function()
-    require("package-info").setup()
+    require("package-info").setup({
+      autostart = true,
+      hide_up_to_date = true,
+    })
   end,
   keys = {
     { "<leader>nu", "<cmd>lua require('package-info').change_version()<cr>", desc = "Update package version" },
