2425fab2a4b1be6f94614a5a52462cc164783e1d
Alex Beauchemin <alexbeauchemin01@gmail.com>
2025-02-16 12:04:50 -0500

    Replace colorizer by mini-hipatterns + disable mini.pairs

--- nvim/.config/nvim/lua/plugins/disabled.lua
+++ nvim/.config/nvim/lua/plugins/disabled.lua
@@ -11,4 +11,5 @@ return {
   { "nvim-telescope/telescope.nvim", enabled = false },
   { "danielfalk/smart-open.nvim", enabled = false },
   { "nvim-neo-tree/neo-tree.nvim", enabled = false },
+  { "echasnovski/mini.pairs", enabled = false },
 }
